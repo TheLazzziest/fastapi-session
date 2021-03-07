ca81619ab55fc6685eb699eecd5e20d9fad15987
Maxim Yakovenko <mxyakovenko9@gmail.com>
2021-03-07 18:56:04 +0300

    Merge tests for AsyncSession

--- tests/test_sessions/test_redis.py
+++
@@ -1,8 +0,0 @@
-from fastapi_session import (
-    AsyncSession,
-    RedisBackend,
-)
-
-
-def test_create_redis_backend(redis_session: AsyncSession):
-    assert isinstance(redis_session._backend, RedisBackend)
